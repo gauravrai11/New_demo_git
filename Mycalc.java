e7d5e895d4350dce9d3dd057dd61ca8a5b929e1c
gr1101 <gauravrai110@gmail.com>
2025-01-25 02:15:07 +0530

    third commit

--- Mycalc.java
+++ Mycalc.java
@@ -9,11 +9,18 @@ public class Mycalc {
     int diff(int a, int b) {
 		return a-b
 	}
+
+    int mulint a, int b) {
+		return a*b
+	}
+
 	
 	public static void main(String[] args) {
 		Mycalc calc = new Mycalc();
 		System.out.println("Sum is "+calc.sum(20, 10));
         System.out.println("Sum is "+calc.diff(20, 10));
+        System.out.println("Sum is "+calc.mul(20, 10));
+
 
 	}
 
